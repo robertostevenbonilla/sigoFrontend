b4e13024177a2ca2da90164896ad515e83392f29
Roberto Bonilla <r.bonilla@robsul.com>
2024-06-03 08:24:06 -0500

    Rutas de los distintos ambientes

--- src/http-common.js
+++ src/http-common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "https://85.209.230.236/apiv1",
-  //baseURL: "https://sigo.robsul.com/apiv1",
+  baseURL: "https://sigo.robsul.com/apiv1",
   //baseURL: "http://localhost:3000/apiv1",
-  baseURL: "https://sigo.goyaexpressdelivery.com/apiv1",
+  //baseURL: "https://sigo.goyaexpressdelivery.com/apiv1",
   headers: {
     "Content-type": "application/json"
   }
